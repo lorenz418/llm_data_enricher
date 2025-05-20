cadcd5f719fba3c8d731b679b4c1495d0d2e57ed
Lorenz <go56rax@mytum.de>
2025-05-20 16:49:40 +0200

    added simple headless scraper

--- backend/app/utils/scrapers.py
+++ backend/app/utils/scrapers.py
@@ -1,7 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
+from helpers import extract_domain, clean_text, filter_search_results
+from scrapegraphai.graphs import SmartScraperGraph
 
-def search_company(search_query: str, max_results: int = 10):
+GRAPH_CONFIG = GRAPH_CONFIG = {
+    "llm": {
+        "model": "ollama/mistral",
+        "model_tokens": 8192
+    },
+    "headless": True,
+    "verbose": False,
+}
+
+
+def search_company(search_query: str, relevant_domains: list[dict], max_results: int = 10):
     search_engine_url = "https://html.duckduckgo.com/html/"
 
     respons = requests.post(
@@ -14,9 +26,8 @@ def search_company(search_query: str, max_results: int = 10):
 
     results = []
 
-    for result_container in soup.find_all('div', class_='result', limit=max_results):
+    for search_ranking ,result_container in enumerate(soup.find_all('div', class_='result', limit=max_results)):
         title_link = result_container.find('a', class_='result__a')
-
 
         if not title_link:
             continue
@@ -25,11 +36,48 @@ def search_company(search_query: str, max_results: int = 10):
         domain = extract_domain(link)
 
         results.append({
-            'title': clean_text(title_link.get_text()),
             'link': link,
             'domain': domain,
+            'search_ranking': search_ranking + 1
         })
 
-    return results
+    return filter_search_results(results, relevant_domains)
 
-   
+
+def scrape_company_data(search_result: list[dict], relevant_domains: list[dict], prompt: str) -> dict:
+    scraped_company_data = []
+    
+    for result in search_result:
+        # Find matching domain from relevant_domains
+        matching_domain = next(
+            (domain for domain in relevant_domains if domain['domain'] in result['domain']),
+            None
+        )
+
+        if matching_domain['headless_parsing']:
+            scraped_data = headless_scraper(result['link'], prompt)
+        
+        
+        scraped_data['relevance'] = matching_domain['relevance']
+        scraped_data['search_ranking'] = result['search_ranking']
+        scraped_data['data_source'] = result['link']
+
+        scraped_company_data.append(scraped_data)
+
+    return scraped_company_data
+
+def headless_scraper(link: str, prompt: str):
+    smart_scraper = SmartScraperGraph(
+        prompt=prompt,
+        source=link,
+        config=GRAPH_CONFIG
+    )
+
+    return smart_scraper.run()
+
+
+serach_results=search_company(search_query="Agrargenossenschaft Bad Dürrenberg e.G.", relevant_domains=[{'domain': 'dasoertliche', 'headless_parsing': True, 'relevance': 1}])
+
+search_result=scrape_company_data(serach_results, relevant_domains=[{'domain': 'dasoertliche', 'headless_parsing': True, 'relevance': 1}], prompt="{phone:string}")
+
+print(search_result)
